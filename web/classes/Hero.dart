ce18164b6f318c4e7ea8bc113f35378e17410973
Lukáš Cír <lukas.cir@ptw.cz>
2014-07-16 15:32:07 +0200

    Updated hero file

--- web/classes/Hero.dart
+++ web/classes/Hero.dart
@@ -5,13 +5,32 @@ class Hero {
   int _positionX = 0;
   int _positionY = 0;
 
-  final int moveStep = 10;
+  final int _moveStep = 10;
 
-  void oneGrateMoveOfHero(direction){
-    if(direction == GameEngine.LEFT){
-
-    } else if(direction == GameEngine.RIGHT){
-
+  void oneGrateMoveOfHero(direction) {
+    //step until screenL < position < screenR
+    if (direction == GameEngine.LEFT) {
+      _positionY -= _moveStep;
+      heroRefresh();
+    } else if (direction == GameEngine.RIGHT) {
+      _positionY += _moveStep;
+      heroRefresh();
     }
+  }
+
+  void heroRefresh() {
+    //Repinting hero point
+  }
+
+  void fireHeroBigLaserThing(){
+
+  }
+
+  void fireHeroIntelligentMissiles(){
+
+  }
+
+  void heroIsAmazingFireBall(){
+
   }
 }
